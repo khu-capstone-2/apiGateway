af1292fb5bbb0f70d911ce300fb050f53bff34e4
CYY1007 <younguk86@gmail.com>
2023-12-08 20:28:33 +0900

    :tada: Set up

---
+++ src/main/java/eureka/apiGateway/web/controller/HealthController.java
@@ -0,0 +1,20 @@
+package eureka.apiGateway.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health(){
+        return "I'm healthy!";
+    }
+
+    @GetMapping("/")
+    public String welcome(){
+        return "I'm api gateway Server!";
+    }
+}
